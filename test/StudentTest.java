cce99b53553080dc3390b4a4931cf34b12dce3e6
Lauren Greene <laurengreene@arizona.edu>
2025-04-24 10:17:22 -0700

    Update StudentTest.java

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
-
-import model.AssgType;
 import model.Assignment;
 import model.Course;
 import model.Student;
